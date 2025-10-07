b338c3d026576e6c80123f63847aa0e1b9cee6ae
dracu-lah <nevilnicks4321@gmail.com>
2025-10-07 09:52:07 +0530

    feat:added seo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import ResumeBuilder from "./components/ResumeBuilder";
 import Footer from "./components/shared/Footer";
 import MobileWebViewWarningModal from "./components/shared/MobileWebViewWarningModal";
-
+import { Helmet, HelmetProvider } from "react-helmet-async";
 const App = () => {
   return (
     <div>
-      <ResumeBuilder />
-      <Footer />
-      <MobileWebViewWarningModal />
+      <HelmetProvider>
+        <Helmet>
+          <title>Resume Builder — Create Your Professional Resume</title>
+          <meta
+            name="description"
+            content="Free online developer friendly resume builder to create ATS friendly resumes instantly. Download in PDF."
+          />
+          <meta property="og:title" content="Resume Builder" />
+          <meta
+            property="og:description"
+            content="Build your resume easily in minutes."
+          />
+          <meta
+            property="og:image"
+            content="https://resumebuilder.js.org/og-image.png"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Helmet>
+        <ResumeBuilder />
+        <Footer />
+        <MobileWebViewWarningModal />
+      </HelmetProvider>
     </div>
   );
 };
