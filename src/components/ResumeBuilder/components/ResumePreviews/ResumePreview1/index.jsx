a721bbbcecb730c0ad484044f70c51e841cca2fa
dracu-lah <110589548+dracu-lah@users.noreply.github.com>
2025-10-26 22:54:22 +0530

    Dev (#12)
    * feat:added designMode in R1
    
    * fix:added designMode to other resume templates
    
    * fix:changed design mode text

--- src/components/ResumeBuilder/components/ResumePreviews/ResumePreview1/index.jsx
+++ src/components/ResumeBuilder/components/ResumePreviews/ResumePreview1/index.jsx
@@ -346,7 +346,7 @@ const ResumePreviewPage = ({ resumeData, setViewMode }) => {
                 }
               }}
             />
-            <Label htmlFor="design-mode">Design Mode</Label>
+            <Label htmlFor="design-mode">Live Text Edit Mode</Label>
           </div>
         </div>
       </div>
